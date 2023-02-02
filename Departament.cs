0e8cd149dd8e1c03bc123e207556c304e76534e5
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-02-02 17:04:50 -0300

    Create Departament.cs

---
+++ Departament.cs
@@ -0,0 +1,19 @@
+
+
+namespace work.Entities
+{
+    class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
